88c4a3e55bcdfe3078b67ae0798495d8a8b4b4cb
rokamba <rokamba92@gmail.com>
2022-04-18 21:50:26 -0500

    Updates

---
+++ precache-manifest.b71e9b66f443717de75bde054ca9a6c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "76820f51a28f3979f335",
+    "url": "/react-website/static/css/main.0b425c3e.chunk.css"
+  },
+  {
+    "revision": "76820f51a28f3979f335",
+    "url": "/react-website/static/js/main.76820f51.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/react-website/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "5d9ef8bfdfbe7b3fb5d9",
+    "url": "/react-website/static/js/2.5d9ef8bf.chunk.js"
+  },
+  {
+    "revision": "962a7c59215f84ccad3e",
+    "url": "/react-website/static/js/runtime~main.962a7c59.js"
+  },
+  {
+    "revision": "20840cc96d282efa358394e4063d0de5",
+    "url": "/react-website/static/media/me.20840cc9.jpg"
+  },
+  {
+    "revision": "332ea0d882790d04d10483f395c18f4b",
+    "url": "/react-website/static/media/Project1.332ea0d8.PNG"
+  },
+  {
+    "revision": "7669a85c521719cdaf1da2ba6ecfb23f",
+    "url": "/react-website/static/media/Project2.7669a85c.PNG"
+  },
+  {
+    "revision": "5ab082dd982f7ee832d116b0b6abbcdc",
+    "url": "/react-website/static/media/Project3.5ab082dd.PNG"
+  },
+  {
+    "revision": "cb09eaac61b71b6c9eccd18c348ea20a",
+    "url": "/react-website/static/media/hero.cb09eaac.jpg"
+  },
+  {
+    "revision": "a4e1fe07bdd27e1f6dc81b8bc906662e",
+    "url": "/react-website/index.html"
+  }
+];
